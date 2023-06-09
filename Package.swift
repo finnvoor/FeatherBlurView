45f026495556b5379bd69eaf5731d60e5457adc6
Finn Voorhees <finnvoorhees@gmail.com>
2023-06-08 22:08:14 -0700

    FeatherBlurView

--- Package.swift
+++ Package.swift
@@ -1,23 +1,10 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FeatherBlurView",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "FeatherBlurView",
-            targets: ["FeatherBlurView"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "FeatherBlurView"),
-        .testTarget(
-            name: "FeatherBlurViewTests",
-            dependencies: ["FeatherBlurView"]),
-    ]
+    platforms: [.iOS(.v17)],
+    products: [.library(name: "FeatherBlurView", targets: ["FeatherBlurView"])],
+    targets: [.target(name: "FeatherBlurView")]
 )
--- Sources/FeatherBlurView/FeatherBlurView.swift
+++ Sources/FeatherBlurView/FeatherBlurView.swift
@@ -1,2 +1,64 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+import UIKit
+
+public class FeatherBlurUIView: UIView {
+    override public class var layerClass: AnyClass {
+        NSClassFromString(["CA", "Backdrop", "Layer"].joined()) ?? CALayer.self
+    }
+
+    public init(radius: CGFloat = 4, startPoint: UnitPoint = .top, endPoint: UnitPoint = .bottom) {
+        super.init(frame: .zero)
+
+        let selectorString = ["filter", "With", "Type", ":"].joined()
+        let selector = Selector(selectorString)
+        guard let filterClass = NSClassFromString(["CA", "Filter"].joined()) as? NSObject.Type,
+              filterClass.responds(to: selector),
+              let variableBlur = filterClass.perform(selector, with: "variableBlur").takeUnretainedValue() as? NSObject
+        else {
+            return
+        }
+
+        guard let mask = ImageRenderer(
+            content: Rectangle()
+                .fill(LinearGradient(colors: [.clear, .black], startPoint: startPoint, endPoint: endPoint))
+                .frame(width: 100, height: 100)
+        ).cgImage else { return }
+
+        variableBlur.setValue(radius, forKey: "inputRadius")
+        variableBlur.setValue(mask, forKey: "inputMaskImage")
+        variableBlur.setValue(true, forKey: "inputNormalizeEdges")
+
+        layer.filters = [variableBlur]
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+public struct FeatherBlurView: UIViewRepresentable {
+    public typealias UIViewType = FeatherBlurUIView
+
+    public var radius: CGFloat = 4
+    public var startPoint: UnitPoint = .top
+    public var endPoint: UnitPoint = .bottom
+
+    public func makeUIView(context _: Context) -> FeatherBlurUIView {
+        FeatherBlurUIView(radius: radius, startPoint: startPoint, endPoint: endPoint)
+    }
+
+    public func updateUIView(_: FeatherBlurUIView, context _: Context) {}
+}
+
+#Preview {
+    ZStack {
+        AsyncImage(url: URL(string: "https://w.wiki/6opG")) { image in
+            image.resizable().aspectRatio(1, contentMode: .fill)
+        } placeholder: { ProgressView() }
+        VStack {
+            Spacer()
+            FeatherBlurView().frame(height: 400)
+        }
+    }.ignoresSafeArea()
+}
